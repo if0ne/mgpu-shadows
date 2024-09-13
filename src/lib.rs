c3e2c0c576c670b026480a18216a643687d681e1
if0ne <pavel.agafonov.al@gmail.com>
2024-09-13 13:37:42 +0300

    Added mock api for mgpu rendering

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,49 @@ pub mod utils;
 
 /*
 
+fn render() {
+    
+    std::thread::scope(|s| {
+        s.spawn(|| {
+            let worker_thread = self.renderer.gpu_secondary.acquire_direct_worker_thread();
+            let cmd_list = worker_thread.acquire_cmd_list();
 
+            ... direct commands
+
+            self.renderer.gpu_secondary.direct_command_queue.push(&cmd_list);
+            self.renderer.gpu_secondary.direct_command_queue.execute(&self.shared_fence);
+        });
+
+        s.spawn(|| {
+            self.renderer.gpu_primary.copy_command_queue.wait(&self.shared_fence);
+            let worker_thread = self.renderer.gpu_primary.acquire_copy_worker_thread();
+            let cmd_list = worker_thread.acquire_cmd_list();
+
+            ... copy commands
+
+            self.renderer.gpu_primary.copy_command_queue.push(&cmd_list);
+            self.renderer.gpu_primary.copy_command_queue.execute(&self.copy_fence);
+        });
+
+        s.spawn(|| {
+            let worker_thread = self.renderer.gpu_primary.acquire_direct_worker_thread();
+            let cmd_list = worker_thread.acquire_cmd_list();
+
+            ... draw commands
+
+            self.renderer.gpu_primary.direct_command_queue.push(&cmd_list);
+            self.renderer.gpu_primary.direct_command_queue.execute(&self.render_fence);
+
+            let cmd_list = worker_thread.acquire_cmd_list();
+
+            ... draw commands
+
+            self.renderer.gpu_primary.direct_command_queue.push(&cmd_list);
+            self.renderer.gpu_primary.direct_command_queue.execute(&self.render_fence);
+        });
+    });
+
+    self.renderer.present();
+}
 
 */
